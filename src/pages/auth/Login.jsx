5a860ddf88d704ee4ff17f1103574e338447ea0c
ozapcr <oza24si@mahasiswa.pcr.ac.id>
2026-04-28 15:36:15 +0700

    Pertemuan 7, Login API + Axios

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,11 +1,87 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BsFillExclamationDiamondFill } from "react-icons/bs";
+import { ImSpinner2 } from "react-icons/im";
+
 export default function Login() {
+    const navigate = useNavigate()
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState("")
+    const [dataForm, setDataForm] = useState({
+        email: "",
+        password: "",
+    })
+
+    const handleChange = (evt) => {
+        const { name, value } = evt.target
+        setDataForm({
+            ...dataForm,
+            [name]: value,
+        })
+    }
+
+    /* process form */
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        setLoading(true)
+        setError(false)
+
+        axios
+            .post("https://dummyjson.com/user/login", {
+                username: dataForm.email,
+                password: dataForm.password,
+            })
+            .then((response) => {
+                // Jika status bukan 200, tampilkan pesan error
+                if (response.status !== 200) {
+                    setError(response.data.message);
+                    return;
+                }
+
+                // Redirect ke dashboard jika login sukses
+                navigate("/");
+            })
+            .catch((err) => {
+                if (err.response) {
+                    setError(err.response.data.message || "An error occurred");
+                } else {
+                    setError(err.message || "An unknown error occurred");
+                }
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+
+    }
+
+    /* error & loading status */
+    const errorInfo = error ? (
+        <div className="bg-red-200 mb-5 p-5 text-sm font-light text-gray-600 rounded flex items-center">
+            <BsFillExclamationDiamondFill className="text-red-600 me-2 text-lg" />
+            {error}
+        </div>
+    ) : null
+
+    const loadingInfo = loading ? (
+        <div className="bg-gray-200 mb-5 p-5 text-sm rounded flex items-center">
+            <ImSpinner2 className="me-2 animate-spin" />
+            Mohon Tunggu...
+        </div>
+    ) : null
+
     return (
         <div>
             <h2 className="text-2xl font-semibold text-gray-700 mb-6 text-center">
                 Welcome Back 👋
             </h2>
 
-            <form>
+            {errorInfo}
+
+            {loadingInfo}
+
+             <form onSubmit={handleSubmit}>
                 <div className="mb-5">
                     <label className="block text-sm font-medium text-gray-700 mb-1">
                         Email Address
@@ -16,6 +92,8 @@ export default function Login() {
                         className="w-full px-4 py-2 bg-gray-50 border border-gray-300 rounded-lg shadow-sm
                             placeholder-gray-400"
                         placeholder="you@example.com"
+                        name="email"
+                        onChange={handleChange}
                     />
                 </div>
                 <div className="mb-6">
@@ -28,6 +106,8 @@ export default function Login() {
                         className="w-full px-4 py-2 bg-gray-50 border border-gray-300 rounded-lg shadow-sm
                             placeholder-gray-400"
                         placeholder="********"
+                        name="password"
+                        onChange={handleChange}
                     />
                 </div>
                 <button
