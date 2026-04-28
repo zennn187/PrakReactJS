42a1e750c3d302c7dd48a76baaca4f46c08abfbd
ozapcr <oza24si@mahasiswa.pcr.ac.id>
2026-04-28 15:18:50 +0700

    Pertemuan 7, Lazy Loading + Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Dashboard from "./pages/Dashboard";
-import "./assets/tailwind.css"
-import Orders from './pages/Orders';
-import Customers from './pages/Customers';
-import { Route, Routes } from 'react-router-dom';
-import NotFound from './pages/NotFound';
+import React, { Suspense, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./assets/tailwind.css";
+import { Route, Routes } from "react-router-dom";
+import Loading from "./components/Loading";
+// import MainLayout from "./layouts/MainLayout";
+// import AuthLayout from "./layouts/AuthLayout";
+// import Orders from "./pages/Orders";
+// import Customers from "./pages/Customers";
+// import NotFound from "./pages/NotFound";
+// import Login from "./pages/auth/Login";
+// import Register from "./pages/auth/Register";
+// import Forgot from "./pages/auth/Forgot";
+
+const Dashboard = React.lazy(() => import("./pages/Dashboard"))
+const NotFound = React.lazy(() => import("./pages/NotFound"))
+const Orders = React.lazy(() => import("./pages/Orders"))
+const Customers = React.lazy(() => import("./pages/Customers"))
+const Login = React.lazy(() => import("./pages/auth/Login"))
+const Register = React.lazy(() => import("./pages/auth/Register"))
+const Forgot = React.lazy(() => import("./pages/auth/Forgot"))
+const AuthLayout = React.lazy(() => import("./layouts/AuthLayout"))
+const MainLayout = React.lazy(() => import("./layouts/MainLayout"))
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/customers" element={<Customers />} />
-      </Route>
+    <Suspense fallback={<Loading />}>
 
-      <Route element={<AuthLayout />}>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot" element={<Forgot />} />
-      </Route>
-    </Routes>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/customers" element={<Customers />} />
+        </Route>
+
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot" element={<Forgot />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
+
 export default App
